4ef7d9d4abce48298cd7fce09597fffb036da6b7
BarenSindToll <anamariavit29@gmail.com>
2025-04-11 19:25:16 +0300

    Get User data
    Added userRoutes and userControllers to verify user data

--- fixed-backend/server.js
+++ fixed-backend/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
 import authRouter from './routes/authRoutes.js';
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000
@@ -17,6 +18,8 @@ app.use(cors({ credentials: true })); //send the cookies in the response from ex
 //API Endpoints
 app.get('/', (req, res) => res.send("API Working"));
 app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter);
+
 
 app.listen(port, () => console.log(`Server started on PORT: ${port}`));
 
