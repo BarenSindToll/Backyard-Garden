9847e9e8a24e99c2b19f21bf11194b55b7cb367d
BarenSindToll <anamariavit29@gmail.com>
2025-04-11 16:06:42 +0300

    Send welcome email
    Added the nodemailer and Brevo SMTP credentials to send a welcome email after register

--- fixed-backend/controllers/authController.js
+++ fixed-backend/controllers/authController.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import userModel from '../models/userModel.js';
+import transporter from '../config/nodemailer.js';
 
 
 export const register = async (req, res) => {
@@ -30,6 +31,15 @@ export const register = async (req, res) => {
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000 //7 days converted in miliseconds
         });
+
+        //sending welcome mail
+        const mailOptions = {
+            from: process.env.SENDER_EMAIL,
+            to: email,
+            subject: 'Welcome to Backyard Garden!',
+            text: `Welcome to Backyard Garden! Your account has been created with email id: ${email}`
+        }
+        await transporter.sendMail(mailOptions);
 
         return res.json({ success: true });
 
